1ea860361fe66711f9e3b610fa1ccc836adbbbec
MOHAK12309 <yashraj63951@gmail.com>
2024-01-05 13:04:11 +0530

    mohak

--- models/SessionsData.js
+++ models/SessionsData.js
@@ -6,30 +6,34 @@ const jwt = require("jsonwebtoken");
 const AppError = require("../utils/appError");
 const crypto = require("crypto");
 
+const customCollectionName = "playground";
+
 const SessionDataScheme1 = new mongoose.Schema({
-    Location:{
-        type: String,
-    },
-    Date:{
-        type: String,
-    },
-    Duration:{
-        type:String
-    },
-    StartTime:{
-        type: String,
-    },
-    EndTime:{
-        type: String,
-    },
-    TotalPlayers:{
-        type: String,
-    },
-    status:{
-        type:Boolean,
-        default:true
-    }
-})
+  Location: {
+    type: String,
+  },
+  Date: {
+    type: String,
+  },
+  Duration: {
+    type: String,
+  },
+  StartTime: {
+    type: String,
+  },
+  EndTime: {
+    type: String,
+  },
+  TotalPlayers: {
+    type: String,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+}, {
+  collection: customCollectionName // Specify the custom collection name here
+});
 
 const SessionDataNow = mongoose.model("SessionDataNow",SessionDataScheme1)
 
